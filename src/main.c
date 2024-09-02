6291375f59606496f46efc81ec6c59b3f3f2f358
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-02 16:15:14 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,43 @@
-#include "/home/user/Documentos/Logger-Analyzer/lib/file_scanner.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include "/home/user/Documentos/Logger-Analyzer/lib/file_scanner.h"
+
+
+void* thread_function(void* arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    tail_file(data->filename, data->target_string, data->command);
+    return NULL;
+}
 
 int main(int argc, char *argv[]) {
     
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <filename> <target_string> <action_command>\n", argv[0]);
+    if (argc != 7) { // Expect 7 arguments: program name, 4 filenames, target string, and action command
+        fprintf(stderr, "Usage: %s <filename1> <filename2> <filename3> <filename4> <target_string> <action_command>\n", argv[0]);
         return 1;
     }
 
-    const char *filename = argv[1];
-    const char *target_string = argv[2];
-    const char *command = argv[3];
+    const char *filenames[4] = {argv[1], argv[2], argv[3], argv[4]};
+    const char *target_string = argv[5];
+    const char *command = argv[6];
 
-    tail_file(filename, target_string, command);
+    pthread_t threads[4];
+    thread_data_t thread_data[4];
+
+    for (int i = 0; i < 4; i++) {
+        thread_data[i].filename = filenames[i];
+        thread_data[i].target_string = target_string;
+        thread_data[i].command = command;
+
+        if (pthread_create(&threads[i], NULL, thread_function, &thread_data[i]) != 0) {
+            perror("Failed to create thread");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
