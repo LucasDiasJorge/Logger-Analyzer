633952770712c36aeac0b24897fafbedccb31d29
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-02 11:30:37 -0300

    Create file scanner as lib

---
+++ lib/file_scanner.h
@@ -0,0 +1,13 @@
+#ifndef FILE_SCANNER_H
+#define FILE_SCANNER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+void tail_file(const char *filename, const char *target);
+
+#endif
