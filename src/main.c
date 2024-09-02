687abff1df0e1c2970f8c99c8434c83f88e8c1b5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-02 15:03:14 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <filename> <target_string>\n", argv[0]);
+    
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <filename> <target_string> <action_command>\n", argv[0]);
         return 1;
     }
 
     const char *filename = argv[1];
     const char *target_string = argv[2];
+    const char *command = argv[3];
 
-    tail_file(filename, target_string);
+    tail_file(filename, target_string, command);
 
     return 0;
 }
