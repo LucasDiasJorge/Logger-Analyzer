97731828e7534e728702591e97ca74a2969bb87b
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-02 16:14:59 -0300

    Update file_scanner.c

--- src/file_scanner.c
+++ src/file_scanner.c
@@ -7,6 +7,12 @@
 
 #define BUFFER_SIZE 1024
 
+typedef struct {
+    const char *filename;
+    const char *target_string;
+    const char *command;
+} thread_data_t;
+
 void tail_file(const char *filename, const char *target_string, const char *command) {
     
     FILE *file;
@@ -19,40 +25,31 @@ void tail_file(const char *filename, const char *target_string, const char *comm
         return;
     }
 
-    // Move to the end of the file initially
     fseek(file, 0, SEEK_END);
     position = ftell(file);
 
     while (1) {
-        // Move to the previous position to read the new data
         fseek(file, position, SEEK_SET);
 
-        // Read and process the new lines
         while (fgets(buffer, BUFFER_SIZE, file) != NULL) {
-            // Print the line for debug purposes
-            printf("Read line: %s", buffer);
+            printf("Read line: %s\n", buffer);
 
-            // Compare the line with the target string
             if (strstr(buffer, target_string) != NULL) {
                 printf("Found the target string: %s\n", target_string);
                 run_command(command);
             }
 
-            // Update the position for the next iteration
             position = ftell(file);
         }
 
-        // Sleep for a second before checking for new lines
         sleep(5);
 
-        // Reopen the file in case it has been modified or rotated
         file = freopen(filename, "r", file);
         if (!file) {
             perror("Failed to reopen file");
             return;
         }
 
-        // Move to the last known position
         fseek(file, position, SEEK_SET);
     }
 
