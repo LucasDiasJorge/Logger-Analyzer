90b12c94d721e68c70c74281b029a9838804db56
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-06 15:29:37 -0300

    Lib linking refactoring

---
+++ src/lib/action.h
@@ -0,0 +1,6 @@
+#ifndef ACTION_H
+#define ACTION_H
+
+int run_command(const char *command);
+
+#endif
---
+++ src/lib/file_scanner.h
@@ -0,0 +1,25 @@
+#ifndef FILE_SCANNER_H
+#define FILE_SCANNER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <limits.h>
+
+#define BUFFER_SIZE 1024
+
+typedef struct {
+    const char *filename;
+    const char *target_string;
+    const char *command;
+} agent;
+
+const char* get_newest_file(const char* dirpath);
+
+void tail_file(const char *filename, const char *target, const char *command);
+
+#endif
